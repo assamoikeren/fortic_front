b3a01327915ca2ad6c98d35b3d4e59f6f22c2bea
assamoikeren <54672518+assamoikeren@users.noreply.github.com>
2020-05-28 16:09:48 +0200

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+function toggle(){
+	var header = document.querySelector("header");
+	header.classList.toggle("active");
+}
+
+window.addEventListener('scroll', () => {
+	circleAppear();
+	changeBodyColor();
+})
+function changeBodyColor(){
+	var body = document.querySelector("body");
+ 	if(window.scrollY > 750)
+  	{
+		body.style.backgroundColor = "#0F78ED";
+	}
+  	if(window.scrollY > 1500)
+  	{
+		body.style.backgroundColor = "#388BE8";
+	}
+}
+
+function circleAppear(){
+	var circle = document.getElementById('go-up');
+ 	if(window.scrollY > screen.height)
+  	{
+		circle.style.display = "flex";
+	}
+	else
+		{
+		circle.style.display = "none";
+	}
+}
+
+function newDoc(){
+ 	window.location.assign("file:///C:/Users/I/Documents/projet%20uvci/responsive%20web%20site/ff.html");
+}
+
